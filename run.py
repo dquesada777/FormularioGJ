9af10f994bd4d150f967c6460974602eec240759
dquesada777 <ingenieriaysoporte@gmail.com>
2025-06-06 20:24:29 -0500

    depuracion archivo run.py

--- run.py
+++ run.py
@@ -2,28 +2,17 @@ from app import create_app
 import os
 import sys
 import traceback
-
-
-try:
     
-    app = create_app()
+app = create_app()
     
-except Exception as e:
-    print(f"ERROR: Ocurrió un error durante create_app(): {e}")
-    traceback.print_exc()
-    sys.exit(1)  # Salida controlada con código de error
-
 if __name__ == '__main__':
     
     # Configuración del host, puerto y modo de depuración
     # Asegurarse de que el host no incluya el protocolo http://
-    host = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')
-    if host.startswith('http://'):
-        host = host.replace('http://', '')
-    
-    port = int(os.environ.get('FLASK_RUN_PORT', 5000))
-    debug_mode = os.environ.get('FLASK_DEBUG', 'True').lower() in ['true', '1', 't']
-    
+    host = '127.0.0.1'
+    debug_mode = True
+    port = 5000
+        
     try:
         app.run(host=host, port=port, debug=debug_mode)
     except Exception as e:
